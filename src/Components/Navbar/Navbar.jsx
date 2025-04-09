34e911175716c38a1e5c442a353207cc8d280fde
sagarsinghthakur002 <sagarsinghthakur002@gmail.com>
2025-04-09 23:00:10 +0545

    feat: file nav

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+        <img />
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Services</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
