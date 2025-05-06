2db5d5c2797dc0c6b2f346558eae789e8fddecbc
sagarsinghthakur002 <sagarsinghthakur002@gmail.com>
2025-05-06 22:15:01 +0545

    Refactor code structure for improved readability and maintainability

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -31,7 +31,7 @@ const Footer = () => {
     <div className="footer-container">
       <footer
         className="w-full h-[40vh] grid md:grid-cols-2 grid-cols-1 justify-center items-center gap-10 p-10 lg:p-20 container  footer">
-       
+
 
         {/* Logo Section */}
         {/* <div
@@ -44,49 +44,49 @@ const Footer = () => {
             className=" h-[150px] "
           /> 
         </div> */}
-        
-          
-           
+
+
+
 
 
 
         {/* About Us Section */}
         <div className="flex flex-row justify-center items-center gap-20  ">
-        <img
+          <img
             src={logo}
             alt="logo"
-            className=" h-[150px] "
-          /> 
+            className=" h-[150px] " data-aos="zoom-in"
+          />
 
 
-        <div
-          className="flex flex-col justify-center items-start gap-5 mr-10"
-          data-aos="fade-right"
-        >
-          <h1 className="text-white text-2xl font-semibold">About Us</h1>
-          <h2 className="text-white text-justify">
-          We are dedicated unconditionally to provide quality education to the 21st century generation while keeping pace with our traditional culture and values.
-          </h2>
+          <div
+            className="flex flex-col justify-center items-start gap-5 mr-10"
+            data-aos="fade-right"
+          >
+            <h1 className="text-white text-2xl font-semibold">About Us</h1>
+            <h2 className="text-white text-justify">
+              We are dedicated unconditionally to provide quality education to the 21st century generation while keeping pace with our traditional culture and values.
+            </h2>
 
 
-          <div className="flex justify-start items-center gap-4 mt-2">
-            <div className="p-3 rounded-xl text-gray-300   hover:text-blue-600 cursor-pointer transform hover:scale-110 transition duration-300 ">
-              <FaFacebookF className="size-8" />
+            <div className="flex justify-start items-center gap-4 mt-2">
+              <div className="p-3 rounded-xl text-white   hover:text-blue-600 cursor-pointer transform hover:scale-110 transition duration-300 ">
+                <FaFacebookF className="size-8" />
+              </div>
+              <div className="p-3 rounded-xl text-white  hover:text-pink-600 cursor-pointer transform hover:scale-110 transition duration-300">
+                <FaInstagram className="size-8" />
+              </div>
+              <div className="p-3 rounded-xl text-white  hover:text-blue-500 cursor-pointer transform hover:scale-110 transition duration-300">
+                <FaTwitter className="size-8" />
+              </div>
+              <div className="p-3 rounded-xl text-white  hover:text-red-600 cursor-pointer transform hover:scale-110 transition duration-300">
+                <FaYoutube className="size-8" />
+              </div>
             </div>
-            <div className="p-3 rounded-xl text-gray-300  hover:text-pink-600 cursor-pointer transform hover:scale-110 transition duration-300">
-              <FaInstagram className="size-8" />
-            </div>
-            <div className="p-3 rounded-xl text-gray-300  hover:text-blue-500 cursor-pointer transform hover:scale-110 transition duration-300">
-              <FaTwitter className="size-8" />
-            </div>
-            <div className="p-3 rounded-xl text-gray-300  hover:text-red-600 cursor-pointer transform hover:scale-110 transition duration-300">
-              <FaYoutube className="size-8" />
-            </div>
+
+
+
           </div>
-
-
-          
-        </div>
         </div>
 
 
@@ -100,30 +100,31 @@ const Footer = () => {
           <div className="flex items-center gap-3">
             <FaBuilding className="text-white size-5" />
             <h2 className="text-slate-200">
-            Seti Opi Marga, Koteshwor-32, Kathmandu, Nepal
+              Seti Opi Marga, Koteshwor-32, Kathmandu, Nepal
             </h2>
           </div>
           <div className="flex items-center gap-3">
-            <FaMobile className=" size-5 text-gray-300 " />
+            <FaMobile className=" size-5 text-white " />
             <h2 className="text-slate-200">+977 9823405745</h2>
           </div>
           <div className="flex items-center gap-3">
-            <FaFax className=" size-5 text-gray-300 " />
+            <FaFax className=" size-5 text-white " />
             <h2 className="text-slate-200">+977-01-5104450</h2>
           </div>
           <div className="flex items-center gap-3">
-            <IoMdMail className=" size-5 text-gray-300 " />
+            <IoMdMail className=" size-5 text-white " />
             <h2 className="text-slate-200">info@triton.edu.np</h2>
           </div>
 
 
-        </div>
-          <div className="flex items-center   ">
-          <h1 className="text-white mb-6  font-semibold">
+        </div >
+        <div className="flex items-center  w-full" >
+          <h1 className="text-white mb-6  font-semibold border-t border-white "  >
             Copyright &copy; Sagar Thakur. All Rights Reserved.
           </h1>
-            </div>
+        </div>
       </footer>
+      
       {/* Scroll to top button */}
       <div className="bg-blue-600  rounded-3xl hover:bg-[#25A4E5] cursor-pointer fixed lg:bottom-12 bottom-6 right-6 h-11 w-11 flex justify-center items-center shadow-lg shadow-blue-500/50 z-10">
 
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,7 +5,7 @@ import { LuMail } from "react-icons/lu";
 
 function Header() {
   return (
-    <div className='header'>
+    <div className='header container'>
         <h2 className='text-amber-300'><LuMail className='mail'/>info@triton</h2>
         <h2 className='text-amber-300'><IoMdCall className='call'/> +977 01-5104450</h2>
     </div>
